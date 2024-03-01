2596fecdf919584f9fb0df40cc9bf6bb6e73ad27
Andrei Sokolov <andreysokolov1307@mail.ru>
2024-03-01 16:07:13 +0200

    initial commit

---
+++ ITunesMovies/Controllers/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  ITunesMovies
+//
+//  Created by Андрей Соколов on 01.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemYellow
+    }
+}
+
